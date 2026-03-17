36badadfc37d131d47275f5f2fe1265812089973
Dương Anh Đức <ducytcg123456@Mac.fpt>
2026-03-17 14:25:49 +0700

    fix/bug

--- server/src/app.ts
+++ server/src/app.ts
@@ -16,6 +16,7 @@ app.use(cors({
     'http://localhost:5173',
     'http://127.0.0.1:5173',
     'http://192.168.1.30:5173',
+    'https://google.openclaw-box.com',
     config.appUrl,
   ],
   credentials: true,
